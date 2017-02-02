5ae55360e405ef5d2a25a5ba93200c306e06fe6a
Daniel <dtv96calibre@gmail.com>
2017-02-01 20:08:40 -0500

    Added visualization with polygon2D

--- cooling.gd
+++ cooling.gd
@@ -23,11 +23,15 @@ func _process(delta):
 	
 func tick(delta):
 	tick_time += delta
+	
+	
 	if tick_time >= tick_length :
 		tick_time = 0
 		tick_action()
 
 func tick_action():
+	# Set color of reactor (move this out of tick function for smooth color shift)
+	get_child(0).set_color(Color(reactor_temp/256, (reactor_temp-100)/256, (reactor_temp - 100)/256, 256/256))
 	print("Reactor temp:", reactor_temp)
 	print("Ambient temp:", ambient_temp)
 
